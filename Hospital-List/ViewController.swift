ab6a61ab6854a670569cdb44ef38e3e37b2ec720
kimkyunghun3 <kimkyunghun3@naver.com>
2022-09-30 19:39:54 +0900

    Initial Commit

---
+++ Hospital-List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hospital-List
+//
+//  Created by Eddy on 2022/09/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
