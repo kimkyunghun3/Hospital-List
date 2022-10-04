e6bf0cd8908a592f6ee054768c569db239eab617
Eddy <kimkyunghun3@naver.com>
2022-10-05 03:19:27 +0900

    refactor: 초기화 코드 변경 및 폴더화

--- Hospital-List/Application/SceneDelegate.swift
+++ Hospital-List/Application/SceneDelegate.swift
@@ -12,10 +12,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        let mainViewModel = MainViewModel(network: Network(), storage: PersistentManager.shared)
+        let viewController = MainViewController(viewModel: mainViewModel)
         window = UIWindow(windowScene: windowScene)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = viewController
         window?.makeKeyAndVisible()
     }
 }
-
