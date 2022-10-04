833e0b8670eecebafac02ca1756f3474f4c05237
Eddy <kimkyunghun3@naver.com>
2022-10-05 03:08:50 +0900

    feat: LocalDB 구현

---
+++ Hospital-List/Storage/HospitalEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  HospitalEntity+CoreDataClass.swift
+//  
+//
+//  Created by Eddy on 2022/10/04.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(HospitalEntity)
+public class HospitalEntity: NSManagedObject {
+
+}
